64e7979efee89e97e4e4a8ab5ccc8a74abfa020c
Bastien <bmoulia@gmail.com>
2024-06-05 22:36:30 +0200

    firebase hosting

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -10,6 +10,6 @@ export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
-    provideClientHydration(), provideFirebaseApp(() => initializeApp({"projectId":"ok-kara-3e914","appId":"1:309713203806:web:d8a944e5b5463a615abacf","storageBucket":"ok-kara-3e914.appspot.com","apiKey":"AIzaSyDEAgvuX-69GQKvZl9kKmi9zBV5fMvMjaQ","authDomain":"ok-kara-3e914.firebaseapp.com","messagingSenderId":"309713203806"})), provideFunctions(() => getFunctions()),
+    provideClientHydration(), provideFirebaseApp(() => initializeApp({"projectId":"ok-kara-3e914","appId":"1:309713203806:web:d8a944e5b5463a615abacf","storageBucket":"ok-kara-3e914.appspot.com","apiKey":"AIzaSyDEAgvuX-69GQKvZl9kKmi9zBV5fMvMjaQ","authDomain":"ok-kara-3e914.firebaseapp.com","messagingSenderId":"309713203806"})), provideFunctions(() => getFunctions()), provideFirebaseApp(() => initializeApp({"projectId":"ok-kara-3e914","appId":"1:309713203806:web:d8a944e5b5463a615abacf","storageBucket":"ok-kara-3e914.appspot.com","apiKey":"AIzaSyDEAgvuX-69GQKvZl9kKmi9zBV5fMvMjaQ","authDomain":"ok-kara-3e914.firebaseapp.com","messagingSenderId":"309713203806"})), provideFunctions(() => getFunctions()),
   ],
 };
